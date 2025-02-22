1e4eac38c79e75bc1e12e42ea8c6806ef50ca1b0
Dang The Hao <dangthehao28@gmail.com>
2025-02-22 22:58:25 +0700

    First commit

---
+++ src/main/java/vn/dangthehao/hotel_booking_management/HotelBookingManagementApplication.java
@@ -0,0 +1,13 @@
+package vn.dangthehao.hotel_booking_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelBookingManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelBookingManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/dangthehao/hotel_booking_management/HotelBookingManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.dangthehao.hotel_booking_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelBookingManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
